50bd7601099d40c327e81ed01fa9c85521336630
Julien <julicogui@gmail.com>
2020-05-03 21:32:14 -0300

    dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,29 @@
+import React, { useState, createContext } from 'react';
+import { Brand } from './Brand';
+import { Container } from './Container';
+import { GlobalStyle } from './GlobalStyle';
+import { Card } from './Card';
+import  Chart  from './Chart';
+import { VirtualizedTable } from './Table';
+import { DarkSwitch } from './DarkSwitch';
+
+export const ThemeContext = createContext();
+
+const Dashboard = () => {
+  const themeState = useState('light');
+  const [theme, setTheme] = themeState;
+  const dark = theme === 'dark';
+  return (
+    <ThemeContext.Provider value={themeState}>
+      <GlobalStyle dark={dark} />
+      <Container>
+        <Brand />
+        <DarkSwitch />
+        <Chart />
+        <VirtualizedTable />
+      </Container>
+    </ThemeContext.Provider>
+  );
+}
+
+export default Dashboard;
